1fc194054051fb183e76dc98b0ea7ff0ba675fee
M&M <15317421+ActualMandM@users.noreply.github.com>
2021-04-13 23:11:08 -0700

    unhardcode stages
    allows the stage to be set via the song's chart file now. adds fallback so that current songs don't immediately break

--- source/ChartingState.hx
+++ source/ChartingState.hx
@@ -116,6 +116,7 @@ class ChartingState extends MusicBeatState
 		{
 			_song = {
 				song: 'Test',
+				stage: 'stage',
 				notes: [],
 				bpm: 150,
 				needsVoices: true,
@@ -232,6 +233,7 @@ class ChartingState extends MusicBeatState
 		{
 			_song.player1 = characters[Std.parseInt(character)];
 		});
+
 		player1DropDown.selectedLabel = _song.player1;
 
 		var player2DropDown = new FlxUIDropDownMenu(140, 100, FlxUIDropDownMenu.makeStrIdLabelArray(characters, true), function(character:String)
@@ -240,6 +242,15 @@ class ChartingState extends MusicBeatState
 		});
 
 		player2DropDown.selectedLabel = _song.player2;
+
+		var stages:Array<String> = CoolUtil.coolTextFile('assets/data/stageList.txt');
+
+		var stageDropDown = new FlxUIDropDownMenu(10, 130, FlxUIDropDownMenu.makeStrIdLabelArray(stages, true), function(stage:String)
+		{
+			_song.stage = stages[Std.parseInt(stage)];
+		});
+
+		stageDropDown.selectedLabel = _song.stage;
 
 		var tab_group_song = new FlxUI(null, UI_box);
 		tab_group_song.name = "Song";
@@ -253,6 +264,8 @@ class ChartingState extends MusicBeatState
 		tab_group_song.add(loadAutosaveBtn);
 		tab_group_song.add(stepperBPM);
 		tab_group_song.add(stepperSpeed);
+		// layering bullshit
+		tab_group_song.add(stageDropDown);
 		tab_group_song.add(player1DropDown);
 		tab_group_song.add(player2DropDown);
 
