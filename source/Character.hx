513f93e3199f03c7e45d64866f8a2c8434cbb877
M&M <15317421+ActualMandM@users.noreply.github.com>
2021-10-02 16:39:55 -0700

    add gf hair loop too

--- source/Character.hx
+++ source/Character.hx
@@ -99,9 +99,11 @@ class Character extends FlxSprite
 				animation.addByIndices('danceLeft', 'GF Dancing Beat Hair blowing CAR', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 				animation.addByIndices('danceRight', 'GF Dancing Beat Hair blowing CAR', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24,
 					false);
+				animation.addByIndices('idleLoop', 'GF Dancing Beat Hair blowing CAR', [10, 11, 12, 25, 26, 27], "", 24, true);
 
 				addOffset('danceLeft', 0);
 				addOffset('danceRight', 0);
+				addOffset('idleLoop', 0);
 
 				playAnim('danceRight');
 
